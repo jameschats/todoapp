014c92b7a5d2a3ec49c68a1310acc4fde97fbd60
James Fredric <jfredric@brightaira.com>
2024-12-05 12:10:54 +0300

    changes commit

---
+++ todo-web/src/app/components/todo/todo.component.ts
@@ -0,0 +1,64 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-todo',
+//   templateUrl: './todo.component.html',
+//   styleUrls: ['./todo.component.css']
+// })
+// export class TodoComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
+
+import { Component, OnInit } from '@angular/core';
+import { TodoItem, TodoService } from '../../services/todo.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+@Component({
+    selector: 'app-todo',
+    templateUrl: './todo.component.html',
+    styleUrls: ['./todo.component.css'],
+    standalone: true, // Mark as standalone
+    imports: [CommonModule, FormsModule,HttpClientModule], // Import FormsModule here
+})
+export class TodoComponent implements OnInit {
+    todos: TodoItem[] = [];
+    newTodoTitle = '';
+
+    constructor(private todoService: TodoService) {}
+
+    ngOnInit(): void {
+        this.loadTodos();
+    }
+
+    loadTodos(): void {
+        this.todoService.getTodos().subscribe((todos) => (this.todos = todos));
+    }
+
+    addTodo(): void {
+        if (this.newTodoTitle.trim()) {
+            const newTodo: TodoItem = {
+                title: this.newTodoTitle.trim(),
+                isCompleted: false,
+            };
+            this.todoService.addTodo(newTodo).subscribe(() => {
+                this.newTodoTitle = '';
+                this.loadTodos();
+            });
+        }
+    }
+
+    toggleComplete(todo: TodoItem): void {
+        todo.isCompleted = !todo.isCompleted;
+        this.todoService.updateTodo(todo).subscribe();
+    }
+
+    deleteTodo(id: number): void {
+        this.todoService.deleteTodo(id).subscribe(() => this.loadTodos());
+    }
+}
---
+++ todo-web/src/app/services/todo.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface TodoItem {
+    id?: number;
+    title: string;
+    isCompleted: boolean;
+}
+
+@Injectable({
+    providedIn: 'root',
+})
+export class TodoService {
+    private apiUrl = 'https://localhost:5001/api/todo'; // Replace with your backend URL
+
+    constructor(private http: HttpClient) {}
+
+    getTodos(): Observable<TodoItem[]> {
+        return this.http.get<TodoItem[]>(this.apiUrl);
+    }
+
+    addTodo(item: TodoItem): Observable<TodoItem> {
+        return this.http.post<TodoItem>(this.apiUrl, item);
+    }
+
+    updateTodo(item: TodoItem): Observable<void> {
+        return this.http.put<void>(`${this.apiUrl}/${item.id}`, item);
+    }
+
+    deleteTodo(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    }
+}
